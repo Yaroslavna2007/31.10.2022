63bf15ad50b44abcaf9e3408189fd1ac7cefcc99
ps <dhjkjdfk/@.1>
2022-11-03 01:54:00 +0300

    /

--- Домашняя работа/main.py
+++ Домашняя работа/main.py
@@ -5,11 +5,10 @@
 n = int(input())
 m = int(input())
 a = []
-for x in range(m):
-    a.append(['*']*n)
-for w in a:
-    for o in w:
-        #if a.index(o)/2 == 0:
-            print(w.index(o))
+for x in range(n):
+    a.append(['.' * m])
+    for w in range(n):
+        if (((x == 0) or (x%2 == 0)) and w%2 == 0) or ((x % 2 != 0) and (w%2 == 0)):
+            a[x][w] = '*'
 for e in a:
     print((' '.join(e)))
